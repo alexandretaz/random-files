413d16a74765b343ed98d40a5fe36f9b9302841a
Fernando Henrique <liquuid@liquuid.net>
2010-03-31 15:18:50 -0300

    java hellp

---
+++ java/hello/Greeter.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Greeter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Greeter
+{
+    public String sayHello()
+    {
+        String message = "Hello, World";
+        return message;
+    }
+}
---
+++ java/hello/GreeterTest.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class GreeterTest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GreeterTest
+{
+  public static void main (String[] args){
+      Greeter worldGreeter = new Greeter();
+      System.out.println(worldGreeter.sayHello());
+    }
+}
