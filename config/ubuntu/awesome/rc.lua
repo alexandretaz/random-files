a6e5b7457437c2a132e4a4d955af87436c109476
FH <liquuid@hacklab.com.br>
2010-05-24 11:33:13 -0300

    tema do awesome

--- config/ubuntu/awesome/rc.lua
+++ config/ubuntu/awesome/rc.lua
@@ -12,7 +12,7 @@ require("debian.menu")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
