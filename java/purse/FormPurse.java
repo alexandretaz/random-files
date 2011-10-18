4ca716bf770164159158cb409d3e4de1fce4d409
Fernando Silva <fernando@liquuid.net>
2011-10-18 00:41:44 -0200

    coisas

--- java/purse/FormPurse.java
+++ java/purse/FormPurse.java
@@ -4,18 +4,30 @@ public class FormPurse
 {
  public static void main(String[] args){
     Purse myPu = new Purse();
-    
+    //Int i = 0 ;
     String input = JOptionPane.showInputDialog("How many nickels?");
     int count = Integer.parseInt(input);
-    myPu.addNickels(count);
+    Coin c = new Coin(0.05);
+    for (int i = 0; i < count ; i++)
+    {
+        myPu.add(c);
+    }
+
     
     input = JOptionPane.showInputDialog("How many dimes?");
     count = Integer.parseInt(input);
-    myPu.addDimes(count);
-    
+    Coin d = new Coin(0.1);
+    for (int i = 0; i < count ; i++)
+    {
+        myPu.add(d);
+    }
     input = JOptionPane.showInputDialog("How many quarters?");
     count = Integer.parseInt(input);
-    myPu.addQuarters(count);
+    Coin e = new Coin(0.25);
+    for (int i = 0; i < count ; i++)
+    {
+        myPu.add(e);
+    }
  
     double totalValue = myPu.getTotal();
     System.out.print("The total is ");
